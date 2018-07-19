bd1eade856a27d41758b9a442fe0e0c613b67d33
Felipe Sousa <felipe.sousa.fs@gmail.com>
2018-07-19 08:44:53 -0300

    ADD: scaffold EventItemType

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :event_item_types
   devise_for :users, controllers: {
       sessions: 'users/sessions',
       registrations: 'users/registrations'
---
+++ test/system/event_item_types_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class EventItemTypesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit event_item_types_url
+  #
+  #   assert_selector "h1", text: "EventItemType"
+  # end
+end
