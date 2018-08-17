bd552eadd5e7edc290dc7e03a2d81cc10c342292
paulorbsfilho <p4ulinho@outlook.com>
2018-08-17 17:28:03 -0300

    UPDATE_formulario_eventos: Campo nome, valor e tipo de atividade obrigatório.

--- app/models/event_item.rb
+++ app/models/event_item.rb
@@ -2,4 +2,8 @@ class EventItem < ApplicationRecord
   belongs_to :event
   belongs_to :event_item_type
   has_many :inscription_items
+
+  validates :name, presence: true
+  validates :value, presence: true
+  validates_numericality_of :value, greater_than: 0
 end
