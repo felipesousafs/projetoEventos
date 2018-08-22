3e4d891aff35513cbd8ffc093a403af0d7f2a6d7
paulorbsfilho <p4ulinho@outlook.com>
2018-08-21 21:38:00 -0300

    ADD_Responsavel - scaffold Item de responsável.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180821233323) do
+ActiveRecord::Schema.define(version: 20180822003447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,15 @@ ActiveRecord::Schema.define(version: 20180821233323) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "attr_name"
+    t.string "attr_value"
+    t.bigint "responsible_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["responsible_id"], name: "index_items_on_responsible_id"
   end
 
   create_table "locations", force: :cascade do |t|
@@ -245,6 +254,7 @@ ActiveRecord::Schema.define(version: 20180821233323) do
   add_foreign_key "inscriptions", "coupoms"
   add_foreign_key "inscriptions", "events"
   add_foreign_key "inscriptions", "users"
+  add_foreign_key "items", "responsibles"
   add_foreign_key "locations", "locations"
   add_foreign_key "moderators", "events"
   add_foreign_key "moderators", "users"
