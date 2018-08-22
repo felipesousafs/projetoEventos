749d561175c205b9b875d1ef0d62f086da66b6c3
paulorbsfilho <p4ulinho@outlook.com>
2018-08-22 16:38:30 -0300

    UPDATE_Formulario_event_item.

--- app/models/event_item.rb
+++ app/models/event_item.rb
@@ -2,7 +2,11 @@ class EventItem < ApplicationRecord
   belongs_to :event
   belongs_to :event_item_type
   belongs_to :location
+  has_many :responsibles
   has_many :inscription_items, :dependent => :destroy
+
+  accepts_nested_attributes_for :responsibles, allow_destroy: true
+  accepts_nested_attributes_for :inscription_items, allow_destroy: true
 
   validates :name, presence: true
   validates :value, presence: true
